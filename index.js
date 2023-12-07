27ca1ce02b71aeee1c03059787ba859a7bd1e815
F.Tsakiris <filippostsak@gmail.com>
2023-12-07 18:20:20 +0200

    new

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-function setRating(ratingValue) {
-  const stars = document.querySelectorAll(".fa-star");
-  stars.forEach((star, index) => {
-    if (index < ratingValue) {
-      star.classList.add("active");
-    } else {
-      star.classList.remove("active");
-    }
+emailjs.init("U4CmSlQI2idZaLqq-"); // Replace with your user ID from EmailJS
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Get the user's email address from the form input
+    var userEmailAddress = document.getElementById("user_email").value;
+
+    // Define the parameters to send to EmailJS
+    var templateParams = {
+      to_email: userEmailAddress,
+    };
+
+    // Send the email using EmailJS
+    emailjs.send("service_qdsr9hd", "template_qpw2wqi", templateParams).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("Email successfully sent!");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        alert("Failed to send the email.");
+      }
+    );
   });
-}
